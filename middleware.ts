184c7c0fd6b19c5435ab3bd879ca680595ce7b73
karim-237 <karimduval20@gmail.com>
2026-01-25 00:09:41 +0100

    update

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { routing } from './i18n/routing'
 
 import NextAuth from 'next-auth'
 import authConfig from './auth.config'
-import { NextResponse } from 'next/server'
 
 const publicPages = [
   '/',
@@ -14,48 +13,40 @@ const publicPages = [
   '/cart/(.*)',
   '/product/(.*)',
   '/page/(.*)',
+  // (/secret requires auth)
 ]
 
 const intlMiddleware = createMiddleware(routing)
 const { auth } = NextAuth(authConfig)
 
 export default auth((req) => {
-  const { pathname } = req.nextUrl
-
-  // ✅ 1. Redirection explicite de / vers /fr (ou defaultLocale)
-  if (pathname === '/') {
-    const defaultLocale = routing.defaultLocale
-    const url = req.nextUrl.clone()
-    url.pathname = `/${defaultLocale}`
-    return NextResponse.redirect(url)
-  }
-
   const publicPathnameRegex = RegExp(
     `^(/(${routing.locales.join('|')}))?(${publicPages
       .flatMap((p) => (p === '/' ? ['', '/'] : p))
       .join('|')})/?$`,
     'i'
   )
-
-  const isPublicPage = publicPathnameRegex.test(pathname)
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname)
 
   if (isPublicPage) {
+    // return NextResponse.next()
     return intlMiddleware(req)
+  } else {
+    if (!req.auth) {
+      const newUrl = new URL(
+        `/sign-in?callbackUrl=${
+          encodeURIComponent(req.nextUrl.pathname) || '/'
+        }`,
+        req.nextUrl.origin
+      )
+      return Response.redirect(newUrl)
+    } else {
+      return intlMiddleware(req)
+    }
   }
-
-  if (!req.auth) {
-    const newUrl = new URL(
-      `/sign-in?callbackUrl=${
-        encodeURIComponent(req.nextUrl.pathname) || '/'
-      }`,
-      req.nextUrl.origin
-    )
-    return NextResponse.redirect(newUrl)
-  }
-
-  return intlMiddleware(req)
 })
 
 export const config = {
+  // Skip all paths that should not be internationalized
   matcher: ['/((?!api|_next|.*\\..*).*)'],
 }
