af4436d45d8d7288daa188ea3301e304d928aa09
karim-237 <karimduval20@gmail.com>
2026-01-25 08:22:16 +0100

    update

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,6 @@
 import { redirect } from 'next/navigation'
 import { routing } from '@/i18n/routing'
 
-export default function Page({ params }: { params: { locale?: string } }) {
-  const locale = routing.locales.includes(params?.locale as string)
-    ? params!.locale!
-    : routing.defaultLocale
-
-  redirect(`/${locale}/sign-in`)
+export default function Page() {
+  redirect(`/${routing.defaultLocale}/sign-in`)
 }
