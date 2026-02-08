5577de9ffa2caa82fcbbe936a35c7951862d70ac
karim-237 <karimduval20@gmail.com>
2026-02-08 13:28:28 +0100

    update

--- app/[locale]/test-setting/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/test-setting/page.tsx
-import { getNoCachedSetting } from '@/lib/actions/setting.actions'
-
-export default async function TestSettingPage() {
-  const setting = await getNoCachedSetting() // <- ici le log va s'afficher dans le terminal
-  return (
-    <pre>{JSON.stringify(setting, null, 2)}</pre>
-  )
-}
--- lib/uploadAuth.ts
+++
@@ -1,5 +0,0 @@
-// lib/uploadAuth.ts
-export const auth = () => {
-  // Retourne true pour permettre l'upload
-  return true
-}
